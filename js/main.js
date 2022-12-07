770f25bde755fedd650c6af67053b76a74137ec4
nargiza-frontend <e.nargiza0592@gmail.com>
2022-12-07 19:57:12 +0500

    Add js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var button = document.querySelector(".prices-btn")
+var pricestype = document.querySelector(".prices-type")
+var color = document.querySelector(".prices-type")
+
+function btnclick(){
+  pricestype.style.backgroundColor = "blue"
+  color.style.color = "white"
+}
